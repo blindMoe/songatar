848c9d4f5a7ba8b1be9341691542b67f8bff9599
Sean Moe <sean@envoy.com>
2020-02-26 13:24:47 -0800

    Add create endpoint verb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  jsonapi_resources :profiles, only: %i[confirm index show update]
+  jsonapi_resources :profiles, only: %i[confirm create index show update]
 end
