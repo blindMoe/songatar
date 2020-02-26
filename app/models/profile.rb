25bf26d4a326b3e3b2e1ed442eb67783d73bae10
Sean Moe <sean@envoy.com>
2020-02-26 13:08:05 -0800

    Update model declaration

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # :nodoc:
-class Profile < ActiveRecord::Model
+class Profile < ApplicationRecord
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :email, uniqueness: true
   validates :token, uniqueness: true
