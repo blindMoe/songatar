e9e129ba305cb824fe60c3c5c43c9881f0c8ef9b
Sean Moe <sean@envoy.com>
2020-02-27 16:48:52 -0800

    Because Ryan did not want to do it right

--- app/resources/profile_resource.rb
+++ app/resources/profile_resource.rb
@@ -3,11 +3,10 @@
 # :nodoc:
 class ProfileResource < ApplicationResource
   attribute :email
+  attribute :spotify_embed_url
   attribute :spotify_embed_url_waiting
+  attribute :spotify_trackid
   attribute :spotify_trackid_waiting
-
-  readonly :spotify_trackid
-  readonly :spotify_embed_url
 
   filter :email
 
